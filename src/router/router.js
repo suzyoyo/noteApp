cd8999f12d7b7f2a990c4b296e2b3e875706ef0d
suzyoyo <xiaoj112133@gmail.com>
2026-09-23 14:38:51 +0800

    Fix GitHub Pages build deployment and routing

--- src/router/router.js
+++ src/router/router.js
@@ -1,4 +1,4 @@
-import { createRouter, createWebHistory } from "vue-router";
+import { createRouter, createWebHashHistory } from "vue-router";
 import NoteGrid from "../components/NoteGrid.vue";
 import AddNote from "../components/AddNote.vue";
 import EditNote from "../components/EditNote.vue";
@@ -10,7 +10,7 @@ const routes = [
 ];
 
 const router = createRouter({
-  history: createWebHistory(),
+  history: createWebHashHistory(import.meta.env.BASE_URL),
   routes,
 });
 
